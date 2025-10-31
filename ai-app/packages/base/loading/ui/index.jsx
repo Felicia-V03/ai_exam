1c68637f8260a5aa2bc7be79144a5bbddd0b7231
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-10-31 19:09:22 +0100

    all styling done

--- ai-app/packages/base/loading/ui/index.jsx
+++ ai-app/packages/base/loading/ui/index.jsx
@@ -2,6 +2,11 @@ import './index.css';
 
 export const Loading = () => {
   return (
-    <div className="loading"></div>
+    <div className="loading">
+      <span className="dot" style={{ '--i': 0 }}></span>
+      <span className="dot" style={{ '--i': 1 }}></span>
+      <span className="dot" style={{ '--i': 2 }}></span>
+      <span className="dot" style={{ '--i': 3 }}></span>
+    </div>
   )
 }
